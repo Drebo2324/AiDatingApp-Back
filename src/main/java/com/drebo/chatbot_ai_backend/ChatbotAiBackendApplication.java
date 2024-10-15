92fea2db9ff932d4f1341345028d26da8848d387
dreBo <drebo2324@gmail.com>
2024-10-14 22:20:48 -0400

    Basic init

---
+++ src/main/java/com/drebo/chatbot_ai_backend/ChatbotAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.drebo.chatbot_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotAiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drebo/chatbot_ai_backend/ChatbotAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drebo.chatbot_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
