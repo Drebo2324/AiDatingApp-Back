b43dbcc581cba61fa98fd2b07c78024c6215feac
dreBo <drebo2324@gmail.com>
2024-10-25 15:41:15 -0400

    getAllMatches endpoint

--- src/main/java/com/drebo/chatbot_ai_backend/matches/MatchController.java
+++ src/main/java/com/drebo/chatbot_ai_backend/matches/MatchController.java
@@ -5,12 +5,14 @@ import com.drebo.chatbot_ai_backend.coversations.ConversationRepo;
 import com.drebo.chatbot_ai_backend.profiles.Profile;
 import com.drebo.chatbot_ai_backend.profiles.ProfileRepo;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -43,5 +45,10 @@ public class MatchController {
         Match match = new Match(UUID.randomUUID().toString(), profile, conversation.id());
         Match savedMatch = matchRepo.save(match);
         return savedMatch;
+    }
+
+    @GetMapping("/matches")
+    public List<Match> getAllMatches(){
+        return matchRepo.findAll();
     }
 }
